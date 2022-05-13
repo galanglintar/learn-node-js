8004d8f210a120c5f6aa2bfac6968a31eb1616b2
Galang Lintar <galanglintar6@gmail.com>
2022-05-13 09:02:25 +0700

    Sending response

--- app.js
+++ app.js
@@ -2,8 +2,18 @@
 // http, https, fs, path, os
 const http = require('http');
 
+/**
+ * @param {http.IncomingMessage} request
+ * * @param {http.ServerResponse} response
+ */
 const requestListener = (request, response) => {
-    console.log(request);
+    // console.log(request);
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html>');
+    response.write('<head><title>First Page</title></head>');
+    response.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    response.write('</html>');
+    response.end();
 }
 
 const server = http.createServer(requestListener);
