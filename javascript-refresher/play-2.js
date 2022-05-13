d2753d61cf87de81b19c5e4314f60f4324eafbe2
Galang Lintar <galanglintar6@gmail.com>
2022-05-13 07:25:05 +0700

    Spread, rest and destructuring

--- javascript-refresher/play-2.js
+++ javascript-refresher/play-2.js
@@ -13,3 +13,11 @@ person.greet();
 const hobbies = ['Sports', 'Cooking'];
 // Note: in js there are some usefull method to playing arround with,
 // use dot [.] to start using one of them
+
+// use spread [...] to pull an array
+// also can user rest [...] to merge multiple array
+const copiedArray = [...hobbies];
+
+// Destructuring object use {}, and array use []
+const {name, age} = person;
+const [h1, h2] = hobbies;
