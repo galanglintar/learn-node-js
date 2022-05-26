431f2d436c885fdcb6d3222db69156682cd6af36
Galang Lintar <galanglintar6@gmail.com>
2022-05-26 11:49:37 +0700

    GraphQl service

--- api/app-rest.js
+++ api/app-rest.js
@@ -4,14 +4,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const graphqlHttp = require("express-graphql").graphqlHTTP;
 
-const graphqlSchema = require("./graphql/schema");
-const graphqlResolver = require("./graphql/resolvers");
-const auth = require("./middleware/auth");
-const { clearImage } = require("./util/file");
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
+
+// for windows install uuid library
+/*
+const { v4: uuidv4 } = require('uuid');
+ 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4())
+    }
+});
+*/
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -48,46 +59,11 @@ app.use((req, res, next) => {
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
   next();
 });
 
-app.use(auth);
-
-app.put("/post-image", (req, res, next) => {
-  if (!req.isAuth) {
-    throw new Error("Not authenticated!");
-  }
-  if (!req.file) {
-    return res.status(200).json({ message: "No file provided!" });
-  }
-  if (req.body.oldPath) {
-    clearImage(req.body.oldPath);
-  }
-  return res
-    .status(201)
-    .json({ message: "File stored.", filePath: req.file.path });
-});
-
-app.use(
-  "/graphql",
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    formatError(err) {
-      if (!err.originalError) {
-        return err;
-      }
-      const data = err.originalError.data;
-      const message = err.message || "An error occurred.";
-      const code = err.originalError.code || 500;
-      return { message: message, status: code, data: data };
-    },
-  })
-);
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -103,6 +79,10 @@ mongoose
     { dbName: "social-network-api" }
   )
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => console.log(err));
